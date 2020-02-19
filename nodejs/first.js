cf8373a117eaa96c0938f7503c63483ffda9969b
1lch2 <1lch2@163.com>
2020-02-19 15:56:24 +1100

    start learning nodejs

---
+++ nodejs/first.js
@@ -0,0 +1,10 @@
+function test(input) {
+    var res = input ** 2
+    return res
+}
+
+console.log(test(5))
+
+var data = require('./data.json')
+console.log(data["key0"])
+console.log(data["key1"])
