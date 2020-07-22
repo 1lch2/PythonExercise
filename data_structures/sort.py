68c7e6e2dcd94dbcc28fed9573589130e73ffad3
1lch2 <1lch2@163.com>
2020-07-22 23:53:30 +1000

    sort update

--- data_structures/sort.py
+++ data_structures/sort.py
@@ -10,11 +10,11 @@ class Sort():
         Returns: 
             The sorted sequence.
         """
-        flag = True
+        flag = True # 是否发生交换
         r_seq = seq.copy()
 
         while flag :
-            flag = False
+            flag = False # 若一趟下来没有发生交换，则 flag 保持 False
             i = 0
             while i < len(r_seq) - 1:
                 temp = r_seq[i]
@@ -42,12 +42,12 @@ class Sort():
         i = low
         j = high
         
-        if low < high: # 递归终止条件：左右端下标相遇
+        if low < high: #* 递归终止条件：左右端下标相遇
             base = seq[low] # 以左端起始的数字为枢轴
             while i < j: # 当左右侧指针相遇时停止循环
                 while seq[j] > base and j > i: # 从右向左扫描比枢轴小的数
                     j -= 1 # j 向左移动一位
-                if j > i:
+                if i < j: # 若左右指针相遇则不进行交换
                     seq[i] = seq[j] # 将比枢轴小的数放到 i 所指的位置
                     i += 1 # i 右移一位
                 
@@ -62,16 +62,18 @@ class Sort():
             self.quicksort(seq, low, i-1) # 递归对枢轴左侧排序
             self.quicksort(seq, i+1, high) # 递归对枢轴右侧排序
     
-    # Merge sort.
-    # Complexity: O(nlogn)
+
     @classmethod
     def mergesort(self, seq: list):
-        s_len = len(seq)
+        """Two-way merge sort. Complexity: O(nlogn)
 
+        Returns:
+            The sorted sequence.
+        """
         # Merge small lists into one ordered sequence.
-        def merge(left:list, right:list)->list :
+        def merge(left: list, right: list) -> list :
             res = []
-            i, j = 0, 0
+            i, j = 0, 0 # 左右列表的下标
             left_len = len(left)
             right_len = len(right)
 
@@ -83,13 +85,14 @@ class Sort():
                     res.append(right[j])
                     j += 1
 
+            # 当某一列表并归完后，合并另一个列表的剩余元素
             if i == left_len:
                 res += right[j:]
             elif j == right_len:
                 res += left[i:]
-            
             return res
-                
+
+        s_len = len(seq)
         if s_len <= 1:
             return seq
         middle = s_len // 2
@@ -125,13 +128,43 @@ class Sort():
 
             i += 1
     
-    # Heap sort
-    # O(nlogn)
+    @classmethod
     def heap_sort(self, seq: list):
-        pass
+        """Max heap sort. Complexity: O(nlogn).
+
+        Args:
+            seq: The input sequence.
+            n: The length of the sequence.
+        """
+        def maxHeap(seq: list, size: int, i: int):
+            # 从0开始的下标
+            largest = i # 当前最大的节点（根节点）下标
+            l = 2 * i + 1 # 左节点下标
+            r = 2 * i + 2 # 右节点下标
+
+            # 下标不能超出范围
+            if l < size and seq[largest] < seq[l]: # 若左节点大于当前最大的节点，将largest指向 左 节点
+                largest = l
+            if r < size and seq[largest] < seq[r]: # 若右节点大于当前最大的节点，将largest指向 右 节点
+                largest = r
+            if largest != i: # 若有子节点比父节点大，则交换最大的节点到根节点
+                seq[i], seq[largest] = seq[largest], seq[i]
+                maxHeap(seq, size, largest) # 递归对调整后的二叉树进行检测调整
+
+        n = len(seq)
+        # 建立初始大顶堆
+        for i in range(n, -1, -1): # 从后往前遍历
+            maxHeap(seq, n, i)
+
+        # 换出根节点的值并将其放入最终位置
+        for i in range(n-1, 0, -1): # 从 倒数第二个位置开始，将已经排到最后的元素排除
+            seq[0], seq[i] = seq[i], seq[0] # 此步将位于第一位的最大元素换到最后
+            maxHeap(seq, i, 0) # 在减少了一个关键字的序列中进行调整
+
 
 if __name__ == '__main__':
     sample = [2, 3, 10, 1, 4, 7, 5, 9, 12, 0, 30, 8]
+
     s = sample.copy()
     Sort.quicksort(s, 0, len(s)-1)
     print(s)
@@ -139,3 +172,8 @@ if __name__ == '__main__':
     s = sample.copy()
     Sort.simple_select_sort(s)
     print(s)
+
+    s = sample.copy()
+    Sort.heap_sort(s)
+    print(s)
+
--- leetcode/sort/215.py
+++ leetcode/sort/215.py
@@ -48,3 +48,63 @@ class Solution:
         或者直接用sort()方法，比自己写的快十倍
         '''
         return res[-k]
+
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 堆排序思路，只排 k 个
+        # 还是比快排慢
+        def maxHeap(seq: list, size: int, i: int):
+            largest = i
+            l = 2*i + 1
+            r = 2*i + 2
+
+            if l < size and seq[l] > seq[largest]:
+                largest = l
+            if r < size and seq[r] > seq[largest]:
+                largest = r
+            if largest != i:
+                seq[largest], seq[i] = seq[i], seq[largest]
+                maxHeap(seq, size, largest)
+        
+        n = len(nums)
+        for i in range(n, -1, -1):
+            maxHeap(nums, n, i)
+        
+        for i in range(n-1, n-k-1, -1):
+            nums[0], nums[i] = nums[i], nums[0]
+            maxHeap(nums, i, 0)
+        return nums[-k]
+
+class Solution3:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 快排思路，每次确定一个元素的位置
+        l = len(nums)
+        left = 0
+        right = l - 1 
+        pivot = self.__partition(nums, 0, l-1)
+        while pivot != l - k: 
+            pivot = self.__partition(nums, left, right)
+            if pivot > l - k: # 若目标在枢轴左侧，则进入左子区间继续一轮快排
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return nums[pivot]
+
+    def __partition(self, seq: list, low: int, high: int):
+        i = low
+        j = high
+
+        base = seq[i]
+        while i < j:
+            while seq[j] > base and i < j:
+                j -= 1
+            if i < j:
+                seq[i] = seq[j]
+                i += 1
+            while seq[i] < base and i < j:
+                i += 1
+            if i < j:
+                seq[j] = seq[i]
+                j -= 1
+        seq[i] = base
+        return i
