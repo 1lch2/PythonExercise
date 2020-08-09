3418abaac218b83a782c75a9dd0e8cf9c89c0a49
1lch2 <1lch2@163.com>
2020-08-09 17:22:02 +1000

    lfu update

--- leetcode/linked_list/460.py
+++ leetcode/linked_list/460.py
@@ -1,3 +1,30 @@
+# 请你为 最不经常使用（LFU）缓存算法设计并实现数据结构。它应该支持以下操作：get 和 put。
+
+# get(key) - 如果键存在于缓存中，则获取键的值（总是正数），否则返回 -1。
+# put(key, value) - 如果键已存在，则变更其值；如果键不存在，请插入键值对。
+# 当缓存达到其容量时，则应该在插入新项之前，使最不经常使用的项无效。
+# 在此问题中，当存在平局（即两个或更多个键具有相同使用频率）时，应该去除最久未使用的键。
+# 「项的使用次数」就是自插入该项以来对其调用 get 和 put 函数的次数之和。使用次数会在对应项被移除后置为 0 。
+
+
+# 进阶：
+# 你是否可以在 O(1) 时间复杂度内执行两项操作？
+
+
+# 示例：
+# LFUCache cache = new LFUCache( 2 /* capacity (缓存容量) */ );
+
+# cache.put(1, 1);
+# cache.put(2, 2);
+# cache.get(1);       // 返回 1
+# cache.put(3, 3);    // 去除 key 2
+# cache.get(2);       // 返回 -1 (未找到key 2)
+# cache.get(3);       // 返回 3
+# cache.put(4, 4);    // 去除 key 1
+# cache.get(1);       // 返回 -1 (未找到 key 1)
+# cache.get(3);       // 返回 3
+# cache.get(4);       // 返回 4
+
 class Node:
     """Dual linked list node object.
     """
@@ -83,14 +110,10 @@ class LFUCache:
         node.next.prev = node.prev
 
         return node
-    
+
     def adjust(self, node: Node):
         """Adjust the position of given node to suit the ascending order of times.
-        
-        With the while operation, the complexity is O(n)
         """
-        #TODO: improve the time complexity to O(1)
-
         temp = self.tail
 
         # Find insert position
@@ -99,3 +122,34 @@ class LFUCache:
         # Check using times and insert the node
         if temp.prev.times <= node.times and node.times < temp.times:
             node.insert(temp)
+
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+if __name__ == "__main__":
+    cache = LFUCache(0)
+    res = []
+
+
+    cache.put(0, 0)
+    # cache.put(2, 2)
+
+    # res.append(cache.get(1))
+    
+    # cache.put(3, 3)
+    
+    # res.append(cache.get(2))
+    # res.append(cache.get(3))
+    
+    # cache.put(4, 4)
+    
+    # res.append(cache.get(1))
+    # res.append(cache.get(3))
+    res.append(cache.get(0))
+
+    print(res)
