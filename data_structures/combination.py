c2d74e3134d6d2b9708f09d867eaf56c70fc665c
1lch2 <1lch2@163.com>
2020-08-12 16:58:24 +1000

    combination update

--- data_structures/combination.py
+++ data_structures/combination.py
@@ -1,36 +1,61 @@
-# Returns full combination of the input list.
-# Reference: http://wuchong.me/blog/2014/07/28/permutation-and-combination-realize/
-def comb(numlist:list):
-    allset = []
-    l_len = len(numlist)
+from typing import List
 
-    n = 1 << l_len
-    for i in range(1, n):
-        t_list = []
-        for j in range(l_len):
-            temp = i
-            if temp & (1 << j):
-                t_list.append(numlist[j])
+class Combination:
+    """Contains some combination algorithms.
+    """
 
-        allset.append(t_list)
+    @staticmethod
+    def fullCombination(nums: List) -> List[List]:
+        """Full combination of the input sequence.
 
-    return allset
+        Bit mask method.
+
+        Args:
+            nums: Input sequence.
+
+        Returns:
+            Full combination including empty set.
+
+        Reference: 
+            http://wuchong.me/blog/2014/07/28/permutation-and-combination-realize/
+        """
+        #* 使用二进制位的迭代来代表对应元素是否在组合中存在
+        res = [] # 不需要提前加入空集
+
+        bitmask = 1 # 用来判断对应数字是否加入的掩码
+        iter_times = bitmask << len(nums) # 循环截止的上限值
+
+        for i in range(iter_times): # 迭代从 0 开始则第一个加入的是空集
+            temp = []
+            for j in range(len(nums)): # 在给定元素数量内循环检测每一位是否要加入
+                if (1 << j) & i: # 若对应位和掩码与结果不为 0 则加入
+                    temp.append(nums[j]) 
+            res.append(temp)
+        return res
+
+    @staticmethod
+    def ZuHeIndex(li):
+        """Another method for creating combination
+
+        Reference:
+            From comment in https://blog.csdn.net/destiny_python/article/details/77461518
+        """
+        reli = []
+        for i in range(0, len(li)):
+            if 0 == i:
+                reli.append([i])
+            else:
+                addli = []
+                addli.append([i])
+                for ii in reli:
+                    addli.append(ii+[i])
+                reli += addli
+        return reli
 
 
-# From comment in https://blog.csdn.net/destiny_python/article/details/77461518
-def ZuHeIndex(li):
-    reli = []
-    for i in range(0, len(li)):
-        if 0 == i:
-            reli.append([i])
-        else:
-            addli = []
-            addli.append([i])
-            for ii in reli:
-                addli.append(ii+[i])
-            reli += addli
-    return reli
-
+def main():
+    res = Combination.fullCombination(["a", "b", "c"])
+    print(res)
 
 if __name__ == '__main__':
-    print(comb(['a', 'b', 'c']))
+    main()
--- leetcode/bit_operation/78.py
+++ leetcode/bit_operation/78.py
@@ -1,22 +1,37 @@
-from typing import List
+# 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
 
+# 说明：解集不能包含重复的子集。
+
+# 示例:
+# 输入: nums = [1,2,3]
+# 输出:
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+
+from typing import List
 
 # Binary bits solution.
 # Reference: http://wuchong.me/blog/2014/07/28/permutation-and-combination-realize/
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        allset = []
-        l_len = len(nums)
+        #* 使用二进制位的迭代来代表对应元素是否在组合中存在
+        res = [] # 不需要提前加入空集
 
-        n = 1 << l_len
-        for i in range(1, n):
-            t_list = []
-            for j in range(l_len):
-                temp = i
-                if temp & (1 << j):
-                    t_list.append(nums[j])
+        bitmask = 1 # 用来判断对应数字是否加入的掩码
+        iter_times = bitmask << len(nums) # 循环截止的上限值
 
-            allset.append(t_list)
-
-        allset.append([])
-        return allset
+        for i in range(iter_times): # 迭代从 0 开始则第一个加入的是空集
+            temp = []
+            for j in range(len(nums)): # 在给定元素数量内循环检测每一位是否要加入
+                if (1 << j) & i: # 若对应位和掩码与结果不为 0 则加入
+                    temp.append(nums[j]) 
+            res.append(temp)
+        return res
